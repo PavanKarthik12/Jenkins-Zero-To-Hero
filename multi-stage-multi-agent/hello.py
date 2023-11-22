1b98b0b3af818a7a8fc12293272d9744758acc44
PavanKarthik12 <40707193+PavanKarthik12@users.noreply.github.com>
2023-11-22 18:16:35 +0530

    Update hello.py

--- multi-stage-multi-agent/hello.py
+++ multi-stage-multi-agent/hello.py
@@ -3,5 +3,3 @@ print("Status code for google.com")
 print(requests.get("https://www.google.com/").status_code)
 
 
-port forward -  kubectl port-forward service/example-argocd-server --address 0.0.0.0 5000:80
-                   
